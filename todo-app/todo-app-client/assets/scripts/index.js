626a628aebc14648b744f3d14ebc6ddd1f6884c8
dananderson <blusplatt@gmail.com>
2022-04-09 17:08:49 +0200

    Adding all build changes

--- todo-app/todo-app-client/assets/scripts/index.js
+++ todo-app/todo-app-client/assets/scripts/index.js
@@ -1,3 +1,52 @@
 (function() {
-   console.log("Running");
+
+   const port = 4000
+   
+   const form = document.getElementById("form");
+   const formSubmitButton = form.querySelector(".submit");
+
+   formSubmitButton.addEventListener("click", (event) => {
+      handleFormSubmit(event);
+   });
+
+   const handleFormSubmit = (event) => {
+      event.preventDefault()
+      
+      const formData = new FormData(form);
+      const inputValue = formData.get("user-input");
+
+      (inputValue) ? validData(inputValue) : invalidData();
+   };
+
+   const validData = (value) => {
+      const processedValue = {
+         "todo": value
+      }
+
+      const response = fetch(`http://localhost:${port}/add-item`, {
+         method: 'POST',
+         mode: 'cors',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(processedValue)
+      });
+      //const data = response.json();
+      //console.log(data);
+   };
+
+   const invalidData = () => {
+      const errorNotification = document.getElementById("error-notification");
+      const message = document.createElement("p");
+
+      message.textContent = "Sorry you input was invalid";
+      errorNotification.appendChild(message);
+      errorNotification.style.display = "block";
+
+      setTimeout(() => {
+         message.remove();
+         errorNotification.style.display = "none";
+      }, 5000);
+   };
+
 } ());
--- todo-app/todo-app-server/app.js
+++ todo-app/todo-app-server/app.js
@@ -1,3 +1,39 @@
+const port = 4000;
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://localhost:27017/";
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 
-//This file will be the server
+app.post("/add-item", (req, res) => {
+   mongoDB_Add(req.body);
+   res.send(req.body);
+});
+
+app.get("/", (req, res) => {
+   res.send("Todo App Server: Hello!");
+});
+
+app.listen(port, () => {
+   console.log(`Todo App server: Listening on port ${port}`);
+});
+
+const mongoDB_Add = async (data) => {
+   MongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+
+      const data_base = db.db("todo-app");
+      const inputData = data;
+
+      data_base.collection("items").insertOne(inputData, (err, res) => {
+        if (err) throw err;
+
+        console.log("MongoDB - One object added to collection: items");
+        db.close();
+      });
+   });
+};
