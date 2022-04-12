0702908ef52e0a302ea9406cf878222f4c77c708
dananderson <blusplatt@gmail.com>
2022-04-12 08:12:30 +0200

    Adding all build changes to vintage-computer

--- vintage-desktop/assets/scripts/desktop.js
+++ vintage-desktop/assets/scripts/desktop.js
@@ -1,5 +1,31 @@
+import { dragElement } from "./drag.js";
+
 (function desktop() {
 
-   console.log("Working");
+   const time_element = document.querySelector(".time--text");
 
+   const desktop_icons = [
+      {icon_element: document.querySelector(".recycle-bin")},
+      {icon_element: document.querySelector(".my-computer")}
+   ]
+
+   const updateTime = () => {
+      let today = new Date();
+      let hours = today.getHours();
+      let minutes = (today.getMinutes().toString().length === 1) ? `0${today.getMinutes()}` : today.getMinutes();
+      let meridiem = (today.getHours() > 11) ? "PM" : "AM";
+      let time = `${hours}:${minutes} ${meridiem}`;
+
+      time_element.textContent = `${time}`;
+      setInterval(updateTime, 10000)
+   };
+
+   const initialLoad = (desktop_icons) => {
+      desktop_icons.forEach(icon => {
+         dragElement(icon.icon_element);
+      });
+   };
+
+   initialLoad(desktop_icons);
+   updateTime();
 } ());
