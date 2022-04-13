57294abf7d8250a4a6ad16bb2488c610a8e7e657
dananderson <blusplatt@gmail.com>
2022-04-13 09:24:52 +0200

    Adding al build changes

--- vintage-desktop/assets/scripts/desktop.js
+++ vintage-desktop/assets/scripts/desktop.js
@@ -2,12 +2,13 @@ import { dragElement } from "./drag.js";
 
 (function desktop() {
 
-   const start_button = document.querySelector(".start");
-   const time_element = document.querySelector(".time--text");
+   const desktop = document.querySelector(".desktop");
    const desktop_icons = [
       {icon_element: document.querySelector(".recycle-bin")},
       {icon_element: document.querySelector(".my-computer")}
    ]
+   const start_button = document.querySelector(".start");
+   const time_element = document.querySelector(".time--text");
 
    const updateTime = () => {
       let today = new Date();
@@ -19,6 +20,24 @@ import { dragElement } from "./drag.js";
       time_element.textContent = `${time}`;
       setInterval(updateTime, 10000)
    };
+
+   desktop.addEventListener("contextmenu", function(event) {
+      event.preventDefault();
+
+      const menu = this.querySelector(".right-click-menu");
+      const mouseX = event.pageX;
+      const mouseY = event.pageY;
+
+      menu.style.top = `${mouseY}px`;
+      menu.style.left = `${mouseX}px`;
+
+      (menu.classList.contains("active")) ? menu.classList.remove("active") : menu.classList.add("active");
+   });
+
+   desktop.addEventListener("click", function() {
+      const menu = this.querySelector(".right-click-menu");
+      if(menu.classList.contains("active")) menu.classList.remove("active");
+   });
 
    start_button.addEventListener("click", function() {
       (this.classList.contains("active")) ? this.classList.remove("active") : this.classList.add("active")
