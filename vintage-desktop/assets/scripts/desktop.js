5747403e83e5a0241ee60612f88aa6793b7063ae
dananderson <blusplatt@gmail.com>
2022-04-15 12:22:21 +0200

    Adding some refactor changes plus the start of some new functianality

--- vintage-desktop/assets/scripts/desktop.js
+++ vintage-desktop/assets/scripts/desktop.js
@@ -3,22 +3,25 @@ import { dragElement } from "./drag.js";
 (function desktop() {
 
    const desktop = document.querySelector(".desktop");
-   const desktop_icons = [
-      {icon_element: document.querySelector(".recycle-bin")},
-      {icon_element: document.querySelector(".my-computer")}
-   ]
+   const desktop_icons = document.querySelectorAll(".desktop--icon");
    const start_button = document.querySelector(".start");
-   const time_element = document.querySelector(".time--text");
 
    const updateTime = () => {
-      let today = new Date();
-      let hours = today.getHours();
-      let minutes = (today.getMinutes().toString().length === 1) ? `0${today.getMinutes()}` : today.getMinutes();
-      let meridiem = (today.getHours() > 11) ? "PM" : "AM";
-      let time = `${hours}:${minutes} ${meridiem}`;
+      const time_element = document.querySelector(".time--text");
+      const today = new Date();
+      const hours = today.getHours();
+      const minutes = (today.getMinutes().toString().length === 1) ? `0${today.getMinutes()}` : today.getMinutes();
+      const meridiem = (today.getHours() > 11) ? "PM" : "AM";
+      const time = `${hours}:${minutes} ${meridiem}`;
 
       time_element.textContent = `${time}`;
       setInterval(updateTime, 10000)
+   };
+
+   const makeDesktopIconsDragable = () => {
+      desktop_icons.forEach(desktop_icon => {
+         dragElement(desktop_icon);
+      });
    };
 
    desktop.addEventListener("contextmenu", function(event) {
@@ -39,16 +42,20 @@ import { dragElement } from "./drag.js";
       if(menu.classList.contains("active")) menu.classList.remove("active");
    });
 
+   desktop_icons.forEach(desktop_icon => {
+      desktop_icon.addEventListener("dblclick", function() {
+         // Double click functionality stills needs work
+      });
+   });
+
    start_button.addEventListener("click", function() {
       (this.classList.contains("active")) ? this.classList.remove("active") : this.classList.add("active")
    });
 
-   const initialLoad = (desktop_icons) => {
-      desktop_icons.forEach(icon => {
-         dragElement(icon.icon_element);
-      });
+   const startUpApp = () => {
+      makeDesktopIconsDragable();
+      updateTime();
    };
 
-   initialLoad(desktop_icons);
-   updateTime();
+   startUpApp();
 } ());
